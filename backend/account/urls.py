83b0e232f3f8fcad7aa5557bff7e647aa7813d43
MuhammedAnasEP <anasep1110@gmail.com>
2024-05-11 11:13:51 +0530

    created backend side in backend created authentication views

---
+++ backend/account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from account import views
+
+app_name = "account"
+
+urlpatterns = [
+    path('login', views.loginView),
+    path('register', views.registerView),
+    path('refresh-token', views.CookieTokenRefreshView.as_view()),
+    path('logout', views.logoutView),
+]
